d7da59af6898ba3f5d420df28dc1f11e2442abe3
CruzAlanna <lannikcru@gmail.com>
2025-04-16 19:00:42 +1000

    Added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { Header } from "./Header";
 import { LandmarksPage } from "./LandmarksPage";
 import { Footer } from "./Footer";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <div style={{ textAlign: 'center' }}>
       <Header />
-      <LandmarksPage />
+      <main>
+        {/* Outlet renders the matching child route component from main.jsx */}
+        <Outlet />
+      </main>
       <Footer />
     </div>
   )
--- src/LandmarksShow.jsx
+++ src/LandmarksShow.jsx
@@ -1,4 +1,11 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { ReviewsList } from "./ReviewsList";
+import { ReviewsNew } from "./ReviewsNew";
+
 export function LandmarksShow({ landmark, onUpdate, onDestroy }) {
+  const [reviews, setReviews] = useState([]);
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const form = event.target;
@@ -15,7 +22,37 @@ export function LandmarksShow({ landmark, onUpdate, onDestroy }) {
 
     const successCallback = () => form.reset();
     onUpdate(landmark, params, successCallback);
+  };
+
+  const handleCreateReview = (landmarkId, params, successCallback) => {
+    axios.post(`http://localhost:3000/api/v1/landmarks/${landmarkId}/reviews`, params)
+    .then((response) => {
+      setReviews([...reviews, response.data]);
+      successCallback();
+    })
+    .catch((error) => {
+      console.log(error);
+    });
   }
+
+  // use effect to fetch reviews whenever the landmark changes
+  useEffect(() => {
+    // only going to fetch the data if there's a valid landmark with a valid id
+    if (landmark && landmark.id) {
+      axios.get(`http://localhost:3000/api/v1/landmarks/${landmark.id}`)
+      .then((response) => {
+        // only update the review state if the response contains reviews
+        if (response.data.reviews) {
+          setReviews(response.data.reviews);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+    }
+    // re-run effect when the landmark changes
+  }, [landmark])
+
   
   return (
     <div>
@@ -29,23 +66,25 @@ export function LandmarksShow({ landmark, onUpdate, onDestroy }) {
         <div>
           Image URL: <input defaultValue={landmark.image_url} name="image_url" type="text" />
         </div>
-        <br></br>
         <div>
           Name: <input defaultValue={landmark.name} name="name" type="text" />
         </div>
-        <br></br>
         <div>
           Location: <input defaultValue={landmark.location} name="location" type="text" />
         </div>
-        <br></br>
         <div>
           Description: <input defaultValue={landmark.description} name="description" type="text" />
         </div>
-        <br></br>
         <button type="submit">Update</button>
       </form>
       <h4>Delete Landmark:</h4>
       <button onClick={() => onDestroy(landmark)}>Remove Landmark</button>
+      <hr></hr>
+      <div>
+        <ReviewsList reviews={reviews} />
+        <h4>Write a Review!</h4>
+        <ReviewsNew landmarkId={landmark.id} onCreateReview={handleCreateReview}/>
+      </div>
     </div>
   )
 }
